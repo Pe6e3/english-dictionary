540c29e1cfd60a8ce18ee0e6f8429f85ad649ded
Pe6e3 <xpe6e3@gmail..com>
2025-11-22 16:52:10 +0100

    Заменены теги code на b для жирного текста в уведомлении

--- auto-deploy.sh
+++ auto-deploy.sh
@@ -197,7 +197,7 @@ send_deploy_notification() {
 • Ветка: $CURRENT_BRANCH
 
 📝 Текст последнего коммита:
-<code>$LAST_COMMIT_BODY</code>
+<b>$LAST_COMMIT_BODY</b>
 
 🖥️ Информация о сервере:
 • Хост: $SERVER_HOSTNAME
