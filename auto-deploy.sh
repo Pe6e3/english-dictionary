ed968fc305578e0eb7cb82bdc4e9a3370ae16355
Pe6e3 <xpe6e3@gmail..com>
2025-11-22 16:26:17 +0100

    Исправлен парсинг CPU и памяти из pm2 list для корректного отображения

--- auto-deploy.sh
+++ auto-deploy.sh
@@ -159,11 +159,12 @@ send_deploy_notification() {
         RESTARTS=$(echo "$PM2_INFO" | grep -E "^│ restarts" | awk -F'│' '{print $3}' | xargs)
         
         # Получаем память и CPU из pm2 list (таблица)
-        PM2_LIST=$(pm2 list | grep "english-backend" | head -1)
+        PM2_LIST=$(pm2 list --no-color | grep "english-backend" | head -1)
         if [ -n "$PM2_LIST" ]; then
-            # pm2 list формат: id name mode ↺ status cpu memory
-            MEMORY=$(echo "$PM2_LIST" | awk '{print $(NF-1)}')
-            CPU=$(echo "$PM2_LIST" | awk '{print $(NF-2)}')
+            # pm2 list формат: id │ name │ namespace │ version │ mode │ pid │ uptime │ ↺ │ status │ cpu │ memory │ user │ watching │
+            # Извлекаем значения, разделенные │
+            CPU=$(echo "$PM2_LIST" | awk -F'│' '{for(i=1;i<=NF;i++) if($i ~ /^[[:space:]]*[0-9]+%/) {gsub(/^[[:space:]]*|[[:space:]]*$/,"",$i); print $i; exit}}')
+            MEMORY=$(echo "$PM2_LIST" | awk -F'│' '{for(i=1;i<=NF;i++) if($i ~ /[0-9]+\.[0-9]+(mb|kb|gb|b)/i) {gsub(/^[[:space:]]*|[[:space:]]*$/,"",$i); print $i; exit}}')
         fi
         
         # Формируем строку статуса с переносами строк
